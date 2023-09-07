525415992b7959be9a1da0e2b57ab1cb32e29c86
Iren Peter <peterirenkat@gmail.com>
2023-09-07 10:33:26 +0200

    updatee

--- js2.js
+++ js2.js
@@ -1,14 +1,567 @@
-for (let i = 0; i < favMovies_Gabor.length; i++) {
-  let genre = "";
-  if (favMovies_Gabor[i].title === "Matrix") {
-    review = "lehetséges";
-  } else  if (favMovies_Gabor[i].title === "Breaking Bad"){
-    review = "valós";
-  } else if(favMovies_Gabor[i].title === "The Walking Dead"){
-    review = "durva";
-  } else {
-    review = "apocaliptic";
-  }
- 
-  favMovies_Gabor[i].review = review;
+//  ********************* ARRAY OF SOLO PROJECTS ********************
+//  My Top 4 Movies - Dorogi Gábor
+let favMovies_Gabor = [
+  {
+    title: "Matrix",
+    year: 1999,
+    rating: 8.7,
+    description: "Virtual reality when computers control people's brains",
+    directors: ["Jhon1", "Victor1"],
+    writers: ["JhonY1", "VictorY1"],
+    stars: ["JhonYous1", "VictorYous1"],
+    genres: ["JhonYousEonus1", "VictorYousEonus1"],
+  },
+  {
+    title: "Breaking Bad",
+    year: 2008,
+    rating: 9.5,
+    description: "A chemistry teacher become to Amphetamine dealer",
+    directors: ["Jhon2", "Victor2"],
+    writers: ["JhonY2", "VictorY2"],
+    stars: ["JhonYous2", "VictorYous2"],
+    genres: ["JhonYousEonus2", "VictorYousEonus2"],
+  },
+  {
+    title: "The Walking Dead",
+    year: 2010,
+    rating: 8.1,
+    description: "The civilization becomes a zombie world",
+    directors: ["Jhon3", "Victor3"],
+    writers: ["JhonY3", "VictorY3"],
+    stars: ["JhonYous3", "VictorYous3"],
+    genres: ["JhonYousEonus3", "VictorYousEonus3"],
+  },
+  {
+    title: "The Book of Eli",
+    year: 2010,
+    rating: 6.8,
+    description: "The post-apocalyptic world is enlightened by the Bible",
+    directors: ["Jhon4", "Victor4"],
+    writers: ["JhonY4", "VictorY4"],
+    stars: ["JhonYous4", "VictorYous4"],
+    genres: ["JhonYousEonus4", "VictorYousEonus4"],
+  },
+];
+
+//  Data Around Us - Dorogi Gábor
+let favoriteBooks_Gabor = [
+  {
+    title: "Crazy Town",
+    author: "Sterling R. Braswell",
+    year: 2008,
+    isNewerThan2000: false,
+    characters: ["Tibi", "Kriszti", "Pisti", "Mari"],
+  },
+  {
+    title: "Speed-Speed-Speedfreak",
+    author: "Mick Farren",
+    year: 2010,
+    isNewerThan2000: false,
+    characters: ["Rózsi", "Feri", "Gizi", "Jani"],
+  },
+];
+
+//  My Top 4 Movies - Irén
+let myFavMovies_Iren = [
+  {
+    title: "Holt költők társasága (Dead Poets Society)",
+    year: 1989,
+    rating: 8.1,
+    description:
+      "Maverick teacher John Keating uses poetry to embolden his boarding school students to new heights of self-expression.",
+    directors: ["Peter Weir", "Somebody"],
+    writers: "Tom Schulman",
+    stars: ["Robin Williams", "Ethan Hawk"],
+    genres: ["comedy", "drama"],
+  },
+
+  {
+    title: "Nekem8 (Nothing to lose)",
+    year: 1997,
+    rating: 6.7,
+    description:
+      "Nick's wife's in bed with his boss. He later gets a gun to his head by a carjacker but steps on the gas pedal. They end up friends after adventures together - holdups, burglary, reckless driving, revenge etc. Twists follow.",
+    directors: "Steve Oedekerk",
+    writers: ["Steve Oedekerk", "SecondWriter"],
+    stars: ["Matin Lawrence", "Tim Robbins"],
+    genres: ["comedy", "action"],
+  },
+
+  {
+    title: "Született gyilkosok (Natural born killers)",
+    year: 1994,
+    rating: 7.2,
+    description:
+      "wo victims of traumatized childhoods become lovers and psychopathic serial murderers irresponsibly glorified by the mass media.",
+    directors: "Oliver Stone",
+    writers: ["Quentin Tarantino", "David Veloz"],
+    stars: ["Woody Harelson", "Juliette Lewis"],
+    genres: ["crime", "action"],
+  },
+
+  {
+    title: "Ne nézz fel (Don't look up)",
+    year: 2021,
+    rating: 7.2,
+    description:
+      "Two low-level astronomers must go on a giant media tour to warn humankind of an approaching comet that will destroy planet Earth.",
+    directors: "Adam McKay",
+    writers: ["Adam McKay", "David Sirota"],
+    stars: ["Leonardo DiCaprio", "Jennifer Lawrence", "Meryl Streep"],
+    genres: ["comedy", "drama"],
+  },
+];
+
+//  Data Around Us  - Irén
+let myFavouriteBooks_Iren = [
+  {
+    title: "A Végtelen Történet",
+    author: "Michael Ende",
+    yearPublication: 1979,
+    characters: ["Atraskó", "Barnabás", "Fuhur", "Morla", "Irgamul"],
+    isNewerThan2000: false,
+  },
+
+  {
+    title: "Dűne",
+    author: "Frank Herbert",
+    yearPublication: 1965,
+    characters: ["Paul", "Jessica", "Stilgar", "Chani", "Alia"],
+    isNewerThan2000: false,
+  },
+];
+
+//  My Top 4 Movies - Viki
+let favMovies_Viki = [
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    rating: 9.3,
+    description:
+      "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
+    directors: ["Frank Darabont"],
+    writers: ["Stephen King", "Frank Darabont"],
+    stars: ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
+    genres: ["Drama"],
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    rating: 9.2,
+    description:
+      "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.",
+    directors: "Francis Ford Coppola",
+    writers: ["Mario Puzo", "Francis Ford Coppola"],
+    stars: ["Marlon Brando", "Al Pacino", "James Caan"],
+    genres: ["Crime", "Drama"],
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    rating: 9.0,
+    description:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    directors: ["Christopher Nolan"],
+    writers: ["Jonathan Nolan", "Christopher Nolan", "David S. Goyer"],
+    stars: ["Christian Bale", "Heath Ledger"],
+    genres: ["Action", "Crime", "Drama"],
+  },
+  {
+    title: "12 Angry Men",
+    year: 1957,
+    rating: 9.0,
+    description:
+      "The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before jumping to a hasty verdict.",
+    directors: ["Sidney Lumet"],
+    writers: ["Reginald Rose"],
+    stars: [
+      "Martin Balsam",
+      "John Fiedler",
+      "Lee J. Cobb",
+      "E.G. Marshall",
+      "Jack Klugman",
+      "Edward Binns",
+    ],
+    genres: ["Crime", "Drama"],
+  },
+];
+//  Data Around Us  - Viki
+let favoriteBooks_Viki = [
+  {
+    title: "SPARE",
+    author: "Prince Harry",
+    year: 2023,
+    isNewerThan2000: true,
+    characters: [
+      "Megan Markel",
+      "Prince Harry",
+      "Kathy Middleton",
+      "King Charles III.",
+    ],
+  },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "Rowling J. K.",
+    year: 1997,
+    isNewerThan2000: false,
+    characters: [
+      "Harry Potter",
+      "Hermione Granger",
+      "Voldemort",
+      "Ron Weasley",
+    ],
+  },
+];
+
+//  My Top 4 Movies - Dóra
+let favMovies_Dora = [
+  {
+    title: "Gone with the Wind ",
+    year: 1939,
+    rating: 8.2,
+    description:
+      "A sheltered and manipulative Southern belle and a roguish profiteer face off in a turbulent romance as the society around them crumbles with the end of slavery and is rebuilt during the Civil War and Reconstruction periods.",
+    directors: ["Victor Fleming", "George Cukor", "Sam Wood"],
+    writers: ["Margaret Mitchell", "Sidney Howard", "Oliver H.P. Garrett"],
+    stars: ["Clark Gable", "Vivien Leigh", "Thomas Mitchell"],
+    genres: ["Drama", "Romance", "War"],
+  },
+  {
+    title: "The Silence of the Lambs",
+    year: 1991,
+    rating: 8.6,
+    description:
+      "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
+    directors: ["Jonathan Demme"],
+    writers: ["Thomas Harris", "Ted Tally"],
+    stars: ["Jodie Foster", "Anthony Hopkins", "Lawrence A. Bonney"],
+    genres: ["Crime", "Drama", "Thiller"],
+  },
+  {
+    title: "Changeling",
+    year: 2008,
+    rating: 7.8,
+    description:
+      "Grief-stricken mother Christine Collins takes on the L.A.P.D. to her own detriment when they try to pass off an obvious impostor as her missing child.",
+    directors: ["Clint Eastwood"],
+    writers: ["J. Michael Straczynski"],
+    stars: ["Angelina Jolie", "Colm Feore", "Amy Ryan"],
+    genres: ["Biography", "Crime", "Drama"],
+  },
+  {
+    title: "Shutter Island",
+    year: 2010,
+    rating: 8.2,
+    description:
+      "Teddy Daniels and Chuck Aule, two US marshals, are sent to an asylum on a remote island in order to investigate the disappearance of a patient, where Teddy uncovers a shocking truth about the place.",
+    directors: ["Martin Scorsese"],
+    writers: ["Laeta Kalogridis", "Dennis Lehane"],
+    stars: ["Leonardo DiCaprio", "Emily Mortimer", "Mark Ruffalo"],
+    genres: ["Mystery", "Thiller"],
+  },
+];
+//  Data Around Us  - Dóra
+let favoriteBooks_Dora = [
+  {
+    title: "Fall of Giants",
+    author: "Ken Follett",
+    year: "2010",
+    isNewerThan2000: true,
+    charachters: [
+      "Ethel Williams",
+      "Billy Williams",
+      "Grigori Peshkov",
+      "Walter von Ulrich",
+    ],
+  },
+  {
+    title: "City of Bones",
+    author: "Cassandra Clare",
+    year: "2007",
+    isNewerThan2000: true,
+    charachters: [
+      "Clary Fray",
+      "Simon Lewis",
+      "Jace Wayland",
+      "Isabelle Lightwood",
+    ],
+  },
+];
+
+// Alap struktúra
+let ourTeam_alap = {
+  name: "IrDoGáVi",
+  members: ["", "", ""],
+};
+
+// Members struktúra
+const members = [
+  {
+    name: "",
+    birthday: {
+      year: 1900,
+      month: 1,
+      day: 10,
+    },
+    location: {
+      country: "",
+      city: "",
+    },
+    codinglevel: 60,
+  },
+];
+
+// Alap struktúrába ágyazott, Members struktúra
+let ourTeam = {
+  name: "IrDoGáVi",
+
+  members: [
+    {
+      name: "Irén",
+      birthday: {
+        year: 1974,
+        month: 6,
+        day: 8,
+      },
+      location: {
+        country: "Hungary",
+        city: "Szigetszentmiklós",
+      },
+      codinglevel: 2,
+    },
+
+    {
+      name: "Gábor",
+      birthday: {
+        year: 1988,
+        month: 3,
+        day: 3,
+      },
+      location: {
+        country: "Hungary",
+        city: "Győr",
+      },
+      codinglevel: 60,
+    },
+
+    {
+      name: "Dóra",
+      birthday: {
+        year: 1993,
+        month: 7,
+        day: 20,
+      },
+      location: {
+        country: "Hungary",
+        city: "Debrecen",
+      },
+      codinglevel: 4,
+    },
+
+    {
+      name: "Viki",
+      birthday: {
+        year: 1985,
+        month: 1,
+        day: 6,
+      },
+      location: {
+        country: "Hungary",
+        city: "Pilisvorosvar",
+      },
+      codinglevel: 5,
+    },
+  ],
+};
+
+//console.log(ourTeam);
+ourTeam.members[0].favorites = {};
+//console.log(ourTeam);
+
+// console.log(ourTeam.members[3]);
+// create favorites key as object in each members object
+
+for (let i = 0; i < ourTeam.members.length; i++) {
+  ourTeam.members[i].favorites = {};
 }
+ourTeam.members[0].favorites = {
+  myFavMovies_Iren,
+  myFavouriteBooks_Iren,
+};
+ourTeam.members[1].favorites = {
+  favMovies_Gabor,
+  favoriteBooks_Gabor,
+};
+ourTeam.members[2].favorites = {
+  favMovies_Dora,
+  favoriteBooks_Dora,
+};
+ourTeam.members[3].favorites = {
+  favMovies_Viki,
+  favoriteBooks_Viki,
+};
+
+// console.log(ourTeam);
+
+// console.log(ourTeam.members[0]);
+// ourTeam.members[0].codinglevel = {level : 5, seniority : "senior"}
+// console.log(ourTeam.members[0]);
+// console.log(ourTeam.members[0]);
+
+// console.log(ourTeam.members)
+// console.log(ourTeam);
+
+//Seniority
+
+for (let i = 0; i < ourTeam.members.length; i++) {
+  let seniority = "";
+  if (ourTeam.members[i].codinglevel < 50) {
+    seniority = "Junior";
+  } else if (
+    ourTeam.members[i].codinglevel >= 50 &&
+    ourTeam.members[i].codinglevel < 75
+  ) {
+    seniority = "Medior";
+  } else {
+    seniority = "Senior";
+  }
+  ourTeam.members[i].codinglevel = {
+    level: ourTeam.members[i].codinglevel,
+    seniority: seniority,
+  };
+}
+
+//review
+
+for (let i = 0; i < myFavouriteBooks_Iren.length; i++) {
+  let review = ""
+  if (myFavouriteBooks_Iren[i].title === "A Végtelen Történet") {
+    review = "izgi";
+  } else  {
+    review = "tanulásgos";
+  } 
+ 
+  myFavouriteBooks_Iren[i].review = review;
+}
+// dóri
+for (let i = 0; i < favoriteBooks_Dora.length; i++) {
+  let review = ""
+  if (favoriteBooks_Dora[i].title === "Fall of Giants") {
+    review = "dejo1";
+  } else  {
+    review = "dejo2";
+  } 
+ 
+  favoriteBooks_Dora.review = review;
+}
+//Viki
+for (let i = 0; i < favoriteBooks_Viki.length; i++) {
+  let review = ""
+  if (favoriteBooks_Viki[i].title === "SPARE") {
+    review = "dejo3";
+  } else  {
+    review = "tanulásgos4";
+  } 
+ 
+  favoriteBooks_Viki[i].review = review;
+}
+//Gábor
+for (let i = 0; i < favoriteBooks_Gabor.length; i++) {
+  let review = ""
+  if (favoriteBooks_Gabor[i].title === "Crazy Town") {
+    review = "izgi5";
+  } else  {
+    review = "tanulásgos6";
+  } 
+ 
+  favoriteBooks_Gabor[i].review = review;
+}
+
+console.log(ourTeam.members[3].favorites);
+
+//delete
+for (const movie of favMovies_Dora) {
+  delete movie.description;
+}
+
+for (const movie of favMovies_Viki) {
+  delete movie.description;
+}
+
+for (const movie of myFavMovies_Iren) {
+  delete movie.description;
+}
+for (const movie of favMovies_Gabor) {
+  delete movie.description;
+}
+
+
+for (const book of favoriteBooks_Dora){
+  delete book.isNewerThan2000;
+}
+ for (const book of favoriteBooks_Viki){
+  delete book.isNewerThan2000;
+}
+for (const book of myFavouriteBooks_Iren){
+  delete book.isNewerThan2000;
+}
+
+for (const book of favoriteBooks_Gabor){
+  delete book.isNewerThan2000;
+}
+
+console.log(ourTeam.members[1].favorites)
+
+
+// counting Team members avarage age
+
+let currantYear = 2023;
+let totalAge = 0;
+for (const cica of ourTeam.members) {
+ totalAge += cica.birthday.year
+}
+let averageAge = totalAge / ourTeam.members.length;
+let summ= currantYear - averageAge
+console.log(summ);
+
+// Youngest and Oldest member
+let youngestMember = ourTeam.members[2];
+for (const kutya of ourTeam.members) {
+  if (kutya.birthday.year > youngestMember.birthday.year){
+    youngestMember = kutya
+    
+  }
+}
+console.log("Youngest Member: ",youngestMember.name);
+
+let oldestMember = ourTeam.members[0];
+for (const kutya of ourTeam.members) {
+  if (kutya.birthday.year < oldestMember.birthday.year){
+    oldestMember = kutya
+    
+  }
+}
+console.log("Oldest Member: ",oldestMember.name);
+
+
+
+// DON'T MODIFY THE CODE BELOW THIS LINE
+
+let toExport;
+
+try {
+  //{ourTeam, averageAge, youngestMember, location, }
+  toExport = [
+    { name: "ourTeam", content: ourTeam, type: "object" },
+    { name: "averageAge", content: averageAge, type: "number" },
+    { name: "averageCodingLevel", content: averageCodingLevel, type: "number" },
+    { name: "youngestMember", content: youngestMember, type: "string" },
+    { name: "oldestMember", content: oldestMember, type: "string" },
+    { name: "location", content: location, type: "object" },
+    { name: "commonGenres", content: commonGenres, type: "object" },
+  ];
+} catch (error) {
+  toExport = { error: error.message };
+}
+
+export { toExport };
