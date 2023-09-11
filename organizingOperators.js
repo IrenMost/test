8fdb6c57b077e0fdcfbbef310f50e89b563c452e
Iren Peter <peterirenkat@gmail.com>
2023-09-11 11:19:36 +0200

    jót játszottam

--- organizingOperators.js
+++ organizingOperators.js
@@ -1,14 +1,50 @@
-function multiplication(x,y,z){
-    console.log(x*y*z)
-}
-multiplication(3,4,2);
+// function multiplication(x,y,z){
+//     console.log(x*y*z)
+// }
+// multiplication(3,4,2);
 
-function greet(name = "Anonymus") {
+// function greet(name = "Anonymus") {
 
-    console.log(`ciao, ${name}!`);
-}
-greet();
-greet("David");
+//     console.log(`ciao, ${name}!`);
+// }
+// greet();
+// greet("David");
 
+//komplex
 
+function getAvailableBooks(books)
+ {
+    let availableBookTitles ="";
+    for (const book of books) {
+        if (book.stock > 0){
 
+            availableBookTitles += `   ${book.title}\n`;
+        }
+    }
+   return `ezek a könyvek elérhetőek:\n${availableBookTitles}`;
+ }
+
+const books = [
+    {title: "majomkirály", author: "nemtom", stock:0},
+    {title: "hahó", author: "barki", stock:3},
+    {title: "gerillaharc", author: "huhh", stock:1}
+]
+
+const availableBookMessage = getAvailableBooks(books);
+console.log(availableBookMessage) ;
+
+// function calculatePrice(item, discount) {
+//     return item.price * (1 - discount);
+// }
+// console.log(calculatePrice(5,5));
+
+function greet(name, callback) {
+    console.log(`Ciao, ${name}!`);
+    callback();
+  }
+  
+  function sayGoodbye() {
+    console.log("Goodbye!");
+  }
+  
+  greet("David", sayGoodbye);
