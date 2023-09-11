cf843bb0bac99b6c8b523992d92ad53249b3519f
Iren Peter <peterirenkat@gmail.com>
2023-09-11 10:05:38 +0200

    izgi

---
+++ organizingOperators.js
@@ -0,0 +1,14 @@
+function multiplication(x,y,z){
+    console.log(x*y*z)
+}
+multiplication(3,4,2);
+
+function greet(name = "Anonymus") {
+
+    console.log(`ciao, ${name}!`);
+}
+greet();
+greet("David");
+
+
+
