af6884d60b971086ba4ce7902d6fcbe758725ae2
Iren Peter <peterirenkat@gmail.com>
2023-09-19 12:33:53 +0200

    iffel

--- js5.js
+++ js5.js
@@ -7,24 +7,28 @@ const conjunctions=["and","for","well","as long as","or"];
 const names=["Jon","Doe","Viki","Irén","Dóra"];
 
 
+// function bandNameGenerator(string) {
+// // (hány betűnől áll a string) 1 : length milyen maradékot ad (%) (maradék lehet 0,1,2,3,4)
+//  let length = string.length;
+// if (length % nouns.length === 0) {
+//     console.log(nouns[0])
+// } else if (length % nouns.length === 1) {
+//     console.log(nouns[1])
+// } else if (length % nouns.length === 2) {
+//     console.log(nouns[2])
+// }else if (length % nouns.length === 3) {
+//     console.log(nouns[3]) 
+//  } else if (length % nouns.length === 4) {
+//     console.log(nouns[4])
+
+// } }
+
 function bandNameGenerator(string) {
-// (hány betűnől áll a string) 1 : length milyen maradékot ad (%) (maradék lehet 0,1,2,3,4)
- let length = string.length;
-if (length % nouns.length === 0) {
-    console.log(nouns[0])
-} else if (length % nouns.length === 1) {
-    console.log(nouns[1])
-} else if (length % nouns.length === 2) {
-    console.log(nouns[2])
-}else if (length % nouns.length === 3) {
-    console.log(nouns[3]) 
- } else if (length % nouns.length === 4) {
-    console.log(nouns[4])
-
-} }
-
-for (const words of nouns) {
-    let length = string.length;
-    if (length % nouns.length === i)
+let length = string.length;
+for (let i = 0; i < nouns.length; i++) {
+    if (length % nouns.length === i) {
+        console.log(nouns[i])
+    }
 }
-bandNameGenerator("mimk");
+}
+bandNameGenerator("fajankó");
